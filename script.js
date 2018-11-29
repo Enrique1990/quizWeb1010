dda14308a667957da8521509d928be706f006ca9
Enrique1990 <ecarrillo@techlaunch.io>
2018-11-29 17:35:39 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// Create a variable and set its value to the answer of a math equation. It should be a number Value
+var num1 = 1;
+var num2 = 2;
+var num3 = 3;
+var num4 = 4;
+var num5 = 5;
+var string6 = 'Watch me count : ${num1}, ${num2}, $num3'
+// Create a variable and set its value to 5 strings being concatenated together using the ‘+’ operator. Each string should be at least 10 characters long. Try and make a real paragraph that a human will be able to read and understand.
+
+// Create a variable and use the ES6 string interpolation (back ticks) to set the value of a variable to a string that includes 5 variables whose values are numbers. Use ${} to use these number variables in the string variable.
+
+// Create an array that has 4 numbers, 2 strings, 3 comparisons, another array with 3 elements inside.
+
+// Create an object that has 4 properties: A number, a string, an array, and an object
+
+// Create 5 variables in a js file that is linked to your html file 
+//1
+var number= 1
+var suma= 1+2;
+
+function adicion (suma){
+    return(suma (1+2));
+}
+//2
+var string = "Enrique Cas";
+var string1 = "is my best";
+var string2 = "friend in";
+var string3 = "this weeks";
+var string4 = "of travels";
+
+
+    document.write("Enrique Cas " +  " is my best " + " friend in " + " this weeks " + " of travels");
+//4
+ // array[1,2,3,4,"carlos","pedro",{1:"yes",2:"no", 3:"maybe"}];
+  //2b
+  var first = document.querySelector("first");
+  var word = document.getElementsByClassName(".word");
+  var words = document.getElementById("#words");
+  //var input = document.createAttribute("input: text");
+  var button = document.getElementById("#boton");
+
+
+  boton.addEventListener("click", () => {
+      boton = input.value;
+  } );
+  document.getElementById("words").innerHTML='<img src="https://s-i.huffpost.com/gen/1758711/images/o-HORSE-facebook.jpg">';
+
+  function doSuff(input, src) {
+    src.addEventListener("click", () => {
+        boton = input.value;
+    } );
+  }
+  function loopIt (num1) {
+      num1 = 10;
+      for(let i = 0; i<=num1.length; i++ ){
+            return true;
+      }  
+      false;
+
+  }
+  
